f80695603cb52cac7806ec131e0e5eec5fefed2f
palenoadv <gejincheng0712@gmail.com>
2025-07-30 13:19:58 +0800

    lab09

--- code/lab/lab09/lab09.scm
+++ code/lab/lab09/lab09.scm
@@ -1,10 +1,19 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2)
+  (cond
+    ((< num1 num2) -1)
+    ((> num1 num2) 1)
+    (else 0)))
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num) 
+  (lambda (x) (+ x num)))
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g) 
+  (lambda (x) (f (g x))))
 
-(define (repeat f n) 'YOUR-CODE-HERE)
+(define (repeat f n)
+  (if (= n 0)
+      (lambda (x) x)
+      (lambda (x) (f ((repeat f (- n 1)) x)))))
 
 (define (max a b)
   (if (> a b)
@@ -16,4 +25,7 @@
       b
       a))
 
-(define (gcd a b) 'YOUR-CODE-HERE)
+(define (gcd a b) 
+  (if (= (modulo (max a b)(min a b)) 0)
+      (min a b)
+      (gcd (modulo (max a b)(min a b)) (min a b))))
